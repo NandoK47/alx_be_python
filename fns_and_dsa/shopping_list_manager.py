fc39a11b346e7712bf4d77f89d6df93f11ed2829
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-10-03 17:29:02 -0700

    updte

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,13 +1,12 @@
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
 
 def main():
-    shopping_list = []
-    
+    shopping_list = []    
     while True:
         display_menu()
         choice = input("Enter your choice: ")
